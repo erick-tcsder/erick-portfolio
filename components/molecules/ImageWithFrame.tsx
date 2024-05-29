13572f0bce5d89b65b44b8d31e5ba8a4a5bfefc2
Erick Fonseca <erickfons00@gmail.com>
2024-05-29 14:37:44 -0500

    fix: image size

--- components/molecules/ImageWithFrame.tsx
+++ components/molecules/ImageWithFrame.tsx
@@ -11,7 +11,7 @@ export interface FrameProps {
 
 export const Frame : React.FC<FrameProps> = (props)=>{
   return (
-    <div className={classNames("absolute aspect-square inset-y-0 lg:inset-x-0 m-auto",props.className)} style={props.style}>
+    <div className={classNames("absolute aspect-square inset-y-0 lg:inset-x-0 m-auto min-h-full min-w-full",props.className)} style={props.style}>
       <span className="absolute bg-transparent border-4 left-0 top-0 right-3 bottom-3 border-dark z-30"/>
       <span className="absolute bg-transparent border-4 left-3 top-3 right-0 bottom-0 border-dark z-30"/>
       <div className="inset-4 absolute z-20">{props.children}</div>
